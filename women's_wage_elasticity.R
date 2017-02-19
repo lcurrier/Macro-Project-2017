d2ffa138cab4954ef527b925ba2aeda5aadaf9b9
lcurrier <lindseyacurrier@gmail.com>
2017-02-19 17:42:04 -0600

    first commits

---
+++ women's_wage_elasticity.R
@@ -0,0 +1,16 @@
+#====================
+# Dynamic Modeling Project 
+#====================
+
+library(readr)
+library(dplyr)
+library(tidyr)
+library(stringr)
+
+setwd("~/Documents/Third Year/second quarter/modern inference/project")
+
+#====================
+# read data
+#====================
+
+df <- read_csv("BestMergedIPUMS.csv")
