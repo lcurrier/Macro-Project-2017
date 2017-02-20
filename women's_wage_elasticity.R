a0266aad675e0c9e990b4d02f636306530cb247d
lcurrier <lindseyacurrier@gmail.com>
2017-02-20 13:20:35 -0600

    new things

--- women's_wage_elasticity.R
+++ women's_wage_elasticity.R
@@ -21,28 +21,40 @@ colnames(df) <- tolower(colnames(df))
 df <- filter(df, !is.na(marbasecidp))
 
 # filtering out people with ages not in our range
-listofpeepswithgoodwages <- unique(df[(df$age <= 54 & 25 <= df$age ),]$cpsid) # all peeps with bad ages
-df <- filter(df, cpsid %in% listofpeepswithgoodwages) 
-
-# filtering out rows with children 
-# idk how to do this :( 
+listofpeepswithgoodages <- unique(df[(df$age <= 54 & 25 <= df$age ),]$cpsid) # all peeps with bad ages
+df <- filter(df, cpsid %in% listofpeepswithgoodages) 
 
 #====================
 # Section 2: filtering out everyone besides married heads of houses and spouses
 #====================
 
+# marst=1:	Married, spouse present
+# relate=0101: Head/householder
+# relate= 0201: Spouse
 df <- df %>%
-  filter(MARST==1) %>%
-  filter(RELATE==101|RELATE==201)
+  filter(marst==1) %>%
+  filter(relate==101|relate==201)
 
 #====================
-# Section 3: Creating Columns for Spouse information 
+# Section 3: filtering out everyone in the armed forces
+#====================
+
+# classwkr=26: Armed forces
+df <- df %>%
+  filter(!(classwkr==26)) 
+
+#====================
+# Section 4: filtering out everyone who's spouse is not longer in the dataset
+#====================
+
+#====================
+# Section 5: Creating Columns for Spouse information 
 #====================
 
 
 
 #====================
-# Section 4: Adjusting wage information for inflation ala 2000
+# Section 6: Adjusting wage information for inflation ala 2000
 #====================
 
 
