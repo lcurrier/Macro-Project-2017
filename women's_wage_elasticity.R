e06120208f74c6e0facd634ad205b2abc426c5bb
Sylvia Klosin <sylviaklosin@wireless-s1-no-150-22-233.uchicago.edu>
2017-02-19 19:07:14 -0600

    updates

--- women's_wage_elasticity.R
+++ women's_wage_elasticity.R
@@ -11,9 +11,36 @@ library(stringr)
 # df <- read_csv("/Users/lindsey/Documents/Third\ Year/second\ quarter/dynamic\ modeling/cps_00006.csv")
 
 # for sylvia 
+df <- read_csv("~/Desktop/cps_00006.csv")
+colnames(df) <- tolower(colnames(df)) 
+#====================
+# Section 1: Creating Subsample of the peeps need 
+#====================
+
+#  filtering out people who dont have march info (sylvia is not 100% if this kosher)
+df <- filter(df, !is.na(marbasecidp))
+
+# filtering out people with ages not in our range
+listofpeepswithgoodwages <- unique(df[(df$age <= 54 & 25 <= df$age ),]$cpsid) # all peeps with bad ages
+df <- filter(df, cpsid %in% listofpeepswithgoodwages) 
+
+# filtering out rows with children 
+# idk how to do this :( 
 
 #====================
-# Clean Data
+# Section 2: Creating Columns for Spouse information 
 #====================
 
-# test change 
+
+
+#====================
+# Section 3: Adjusting wage information for inflation ala 2000
+#====================
+
+
+
+
+
+
+
+
