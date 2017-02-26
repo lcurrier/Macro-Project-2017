842a71f8c3bc180c4d5c499fef999d33a4203c59
lcurrier <lindseyacurrier@gmail.com>
2017-02-26 17:43:21 -0600

    just asec, and code to get weight years evenly

--- women's_wage_elasticity.R
+++ women's_wage_elasticity.R
@@ -18,7 +18,11 @@ colnames(df) <- tolower(colnames(df))
 #====================
 
 #  filtering out people who dont have march info (sylvia is not 100% if this kosher)
-df <- filter(df, !is.na(marbasecidp))
+df <- filter(df, !is.na(marbasecidp)) 
+# lindsey thinks we should just use the supplement
+# since this has the march basic info we downloaded as well
+# so this would be the code instead:
+df <- filter(df, asecflag==1) 
 
 # filtering out people with ages not in our range
 listofpeepswithgoodages <- unique(df[(df$age <= 54 & 25 <= df$age ),]$cpsid) # all peeps with bad ages
@@ -44,23 +48,36 @@ df <- df %>%
   filter(!(classwkr==26)) 
 
 #====================
-# Section 4: filtering out everyone who's spouse is not longer in the dataset
-#====================
-
-#====================
-# Section 5: Creating Columns for Spouse information 
+# Section 4: Creating Columns for Spouse information 
 #====================
 
 
+#====================
+# Section 5: Adjusting wage information for inflation ala 2000
+#====================
+
 
 #====================
-# Section 6: Adjusting wage information for inflation ala 2000
+# Section 6: Adjusting sample weight so every year has the same weight
 #====================
 
+newwt <- df %>% group_by(year) %>% 
+  summarize(sumw = sum(as.numeric(wtsupp))) %>%
+  mutate(scale = sumw[1]/sumw)
+df <- df %>% left_join(newwt) %>%
+  mutate(wtsupp2 = scale*as.numeric(wtsupp))
 
+#====================
+# Section 7: Imputing wages 
+#====================
+  
+df <- df %>% mutate(period=ifelse(year<=1981,"79-81",ifelse(year<=1991,"89-91",ifelse(year<=2001,"99-01","09-11")))) %>%
+  mutate(wkswork3 = ifelse(wkswork1<=20,0,1))
 
-
-
+# the regression is going to be: 
+# lm(hourwage ~ factor(period) + factor(sex) + factor(wkswork3), df)
+sketchywage <- lm(hourwage ~ factor(period) + factor(sex) + factor(wkswork3), df)
+# but we need a better measure of average hourly wage since hourwage isnt complete
 
 
 
