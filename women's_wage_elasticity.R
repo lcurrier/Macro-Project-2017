cdfce085c471785a50d93fa9dad0659e3ad62f02
Sylvia Klosin <sylviaklosin@wireless-s1-no-150-20-96.uchicago.edu>
2017-03-03 18:25:27 -0600

    hi

--- women's_wage_elasticity.R
+++ women's_wage_elasticity.R
@@ -13,12 +13,13 @@ library(stringr)
 # for sylvia 
 df <- read_csv("~/Desktop/cps_00006.csv")
 colnames(df) <- tolower(colnames(df)) 
+# varriable to be converted 
+
 #====================
 # Section 1: Creating Subsample of the peeps need 
 #====================
 
-#  filtering out people who dont have march info (sylvia is not 100% if this kosher)
-df <- filter(df, !is.na(marbasecidp)) 
+
 # lindsey thinks we should just use the supplement
 # since this has the march basic info we downloaded as well
 # so this would be the code instead:
@@ -55,7 +56,18 @@ df <- df %>%
 #====================
 # Section 5: Adjusting wage information for inflation ala 2000
 #====================
+# 1 dollar in 2000 was this much, 
+inflation <- data.frame(year = c(1979, 1980, 1981, 1989, 1990, 1991, 1999, 
+                                 2000, 2001, 2009, 2010, 2011) ,
+                        price = c(2.37, 2.09, 1.89, 1.39, 1.32, 1.26, 1.03, 1.00, 
+                                  0.97, 0.80, 0.79, 0.77))
+df$hourwage <- (df$hourwage)
 
+foo <- c(12, 22, 33)
+names(foo) <- c("tic", "tac", "toe")
+foo[["tic"]]
+
+for 
 
 #====================
 # Section 6: Adjusting sample weight so every year has the same weight
